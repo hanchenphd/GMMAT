4652472ae558802d0bfc17a5d2b20a8234062118
Han Chen <han.chen.2@uth.tmc.edu>
2023-04-14 16:30:00 -0500

    Improved error control in glmmkin
    Allowed random.slope for cross-sectional data from related individuals.

--- R/glmmkin.R
+++ R/glmmkin.R
@@ -59,7 +59,7 @@ glmmkin <- function(fixed, data = parent.frame(), kins = NULL, id, random.slope 
       else kins <- list(kins1 = as(diag(length(unique(data[idx, id]))), "sparseMatrix"))
       rownames(kins[[length(kins)]]) <- colnames(kins[[length(kins)]]) <- unique(data[idx, id])
     }
-  } else if(!is.null(random.slope)) stop("Error: no duplicated \"id\" found, \"random.slope\" must be used for longitudinal data with duplicated \"id\".")
+  } else if(!is.null(random.slope) && is.null(kins)) warning("Argument \"random.slope\" ignored for cross-sectional data from unrelated individuals...", call. = FALSE)
   if(inherits(kins, "matrix")) {
     match.idx1 <- match(data[idx, id], rownames(kins))
     match.idx2 <- match(data[idx, id], colnames(kins))
